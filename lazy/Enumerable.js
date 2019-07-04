480a6ed92472c442460b734afb8e14b1473afa6a
Deputatov Nikita <deputatovn@yandex.ru>
2019-07-04 15:28:23 +0300

    add teacher solution

--- lazy/Enumerable.js
+++ lazy/Enumerable.js
@@ -40,3 +40,45 @@ class Enumerable {
     }
     // END
 }
+
+// teacher solution
+class Enumerable {
+    constructor(collection, operations) {
+        this.collection = collection;
+        this.operations = operations || [];
+    }
+
+    build(fn) {
+        return new Enumerable(this.collection.slice(), this.operations.concat(fn));
+    }
+
+    select(fn) {
+        return this.build(coll => coll.map(fn));
+    }
+
+    orderBy(fn, direction = 'asc') {
+        const compareResult = direction === 'asc' ? 1 : -1;
+        const comparator = (a, b) => {
+            const a1 = fn(a);
+            const b1 = fn(b);
+
+
+            if (a1 > b1) {
+                return compareResult;
+            } else if (a1 < b1) {
+                return -compareResult;
+            }
+
+            return 0;
+        };
+        return this.build(coll => coll.sort(comparator));
+    }
+
+    where(fn) {
+        return this.build(coll => coll.filter(fn));
+    }
+
+    toArray() {
+        return this.operations.reduce((acc, func) => func(acc), this.collection);
+    }
+}
